259688cc8b34d64184c89165a50200e3ba8cd01a
JulioOrtizPol <julio5001@hotmail.com>
2023-05-05 16:23:21 -0400

    feat: created list with movie card component

--- App.tsx
+++ App.tsx
@@ -1,118 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useState, useCallback, useEffect } from 'react';
+import { StyleSheet, SafeAreaView, FlatList, View, Text, Image } from 'react-native';
+import { Movies } from './Models/Movie';
+import { MovieCard } from './Components/MovieCard';
+import { MOVIES_API_BASE_URL, MOVIES_API_KEY } from "@env"
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const URL = `${MOVIES_API_BASE_URL}/now_playing?api_key=${MOVIES_API_KEY}&language=en-US&page=1`;
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movies>();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const handleFetchMovies = useCallback(async () => {
+    const response = await fetch(URL);
+    console.log(movies)
+    if (response.ok) {
+      const movies = await response.json();
+      setMovies(movies.results);
+    }
+  }, []);
+  
+  useEffect(() => {
+    handleFetchMovies();
+  }, []);
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+  return <SafeAreaView style={styles.container} > 
+    <FlatList
+        style={{flex:1}}
+        data={movies}
+        renderItem={({item}) => 
+        <MovieCard 
+        id={item.id}
+        title={item.title} 
+        vote_average={item.vote_average}
+        poster_path={item.poster_path}
+        release_date={item.release_date}
+        />
+      }
+        keyExtractor={item => item.id.toString()}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+  </SafeAreaView>;
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  container: {
+    flex: 1,
+    backgroundColor: '#100e2a',
+  }
 });
-
-export default App;
