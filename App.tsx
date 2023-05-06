3fb4275e2afb992d056365a03d0411cecd3c79d1
JulioOrtizPol <julio5001@hotmail.com>
2023-05-06 10:34:12 -0400

    feat: change default language to spanish

--- App.tsx
+++ App.tsx
@@ -4,14 +4,14 @@ import { Movies } from './Models/Movie';
 import { MovieCard } from './Components/MovieCard';
 import { MOVIES_API_BASE_URL, MOVIES_API_KEY } from "@env"
 
-const URL = `${MOVIES_API_BASE_URL}/now_playing?api_key=${MOVIES_API_KEY}&language=en-US&page=1`;
+const apiLanguage = 'es'
+const URL = `${MOVIES_API_BASE_URL}/now_playing?api_key=${MOVIES_API_KEY}&language=${apiLanguage}S&page=1`;
 
 export const App: React.FC = () => {
   const [movies, setMovies] = useState<Movies>();
 
   const handleFetchMovies = useCallback(async () => {
     const response = await fetch(URL);
-    console.log(movies)
     if (response.ok) {
       const movies = await response.json();
       setMovies(movies.results);
